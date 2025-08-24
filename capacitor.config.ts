1ee1e67a347c86884c18436584e840e5717efa59
msdio <dev.msdio@gmail.com>
2025-08-24 09:39:54 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'room-escape-map',
+  webDir: 'dist'
+};
+
+export default config;
